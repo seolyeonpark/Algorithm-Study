6ac195e51f046c9f5a14d5e17d5749c1e535ebb6
seolyeonpark <seolyeonpark@users.noreply.github.com>
2021-01-17 04:44:22 +0900

    No.1

---
+++ 03_박설연/week1/2588.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	int a, b, c, d;
+	cin >> x;
+	cin >> y;
+	a = x * ((y % 100) % 10);
+	b = x * ((y % 100) / 10);
+	c = x * (y / 100);
+	d = x * y;
+	cout << a << endl << b << endl << c << endl << d << endl;
+
+	return 0;
+}
