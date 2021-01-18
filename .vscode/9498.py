573d303b3035ae943e5c152376d2e4b173ffc385
JOLLA99 <yoon6148@daum.net>
2021-01-18 17:11:07 +0900

    1week_two

---
+++ .vscode/9498.py
@@ -0,0 +1,12 @@
+grade = int(input())
+
+if 90<=grade and grade<=100:
+    print("A")
+elif 80<=grade and grade<90:
+    print("B")
+elif 70<=grade and grade<80:
+    print("C")
+elif 60<=grade and grade<70:
+    print("D")
+else:
+    print("F")
