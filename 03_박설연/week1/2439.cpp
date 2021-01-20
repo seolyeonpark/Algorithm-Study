6474073520805c881062d9ab6d34a3739f29452c
seolyeonpark <seolyeonpark@users.noreply.github.com>
2021-01-20 13:00:20 +0900

    No.2 & No.3

---
+++ 03_박설연/week1/2439.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int j;
+		for (j = 0; j < N - i; j++) {
+			cout << " ";
+		}
+		for (j = 0; j < i+1; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	
+	return 0;
+}
---
+++ 03_박설연/week1/2884.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int H;
+	int M;
+
+	cin >> H >> M;
+	
+	if (M > 45) {
+		M -= 45;
+	}
+	else {
+		if(H == 0){
+			H = 23;
+			M += 15;
+
+		}
+
+
+		else {
+			H -= 1;
+			M += 15;
+		}
+	}
+
+	cout << H << " " << M << endl;
+}
