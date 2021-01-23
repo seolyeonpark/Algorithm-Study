476a39bfe7e95d4aada106cd6d4001c2cb419277
seolyeonpark <seolyeonpark@users.noreply.github.com>
2021-01-23 22:04:27 +0900

    No.6 -1

---
+++ 03_박설연/week2/15596.cpp
@@ -0,0 +1,14 @@
+﻿//함수 구현 알고리즘
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long sum(vector<int>& a) {
+	long long num = 0;
+
+	for (int i = 0; i < a.size(); i++) {
+		num += a[i];
+	}
+	return num;
+}
