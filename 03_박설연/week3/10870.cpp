ed1fb6e25b41e4430da7d1a0df9e3b66bbae3761
seolyeonpark <seolyeonpark@users.noreply.github.com>
2021-01-29 22:34:40 +0900

    No.10

---
+++ 03_박설연/week3/10870.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int F(int n) {
+    if (n == 0 || n == 1) 
+        return n;
+    else
+        return F(n - 1) + F(n - 2);
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << F(n);
+    return 0;
+}
---
+++ 03_박설연/week3/10872.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	
+	int tmp = 1;
+	for (int i = 1; i <= N; i++) {
+		tmp *= i;
+	}
+	cout << tmp;
+
+	return 0;
+}
