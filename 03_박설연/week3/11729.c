82d1f87af58ec3f73ff375883a735efc74aa2426
seolyeonpark <seolyeonpark@users.noreply.github.com>
2021-02-01 23:17:13 +0900

    No.10-3

---
+++ 03_박설연/week3/11729.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+#pragma warning(disable:4996)
+
+
+void hanoi(int n, int s, int m, int e)
+{
+    if (n == 1)
+    {
+        printf("%d %d\n", s, e);
+        return;
+    }
+    hanoi(n - 1, s, e, m);
+    hanoi(1, s, m, e);
+    hanoi(n - 1, m, s, e);
+}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int K = pow(2, N) - 1;
+    printf("%d\n", K);
+    hanoi(N, 1, 2, 3);
+
+    return 0;
+}
