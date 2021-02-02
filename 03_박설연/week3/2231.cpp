f77c8b9766cd547a04b8078b5986f5ef2c70863e
seolyeonpark <seolyeonpark@users.noreply.github.com>
2021-02-02 23:05:02 +0900

    no.11

---
+++ 03_박설연/week3/2231.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int N;
+
+int f(void){
+    int creator = 1; 
+    while (1){
+        int copy = creator;
+        int sum = creator; 
+        while (copy) {
+            sum += copy % 10;
+            copy /= 10;
+        }
+        if (creator == N || sum == N)
+            break;
+        creator++;
+    }
+    return creator;
+}
+
+
+
+int main(void){
+    cin >> N;
+    int result = f();
+    if (result == N)
+        cout << 0 << endl;
+    else
+        cout << result << endl;
+    return 0;
+}
